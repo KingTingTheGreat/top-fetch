294e7d4fd212383f16ef7a29e853cda59e446baa
Jeffrey <kingtingthegreat@gmail.com>
2025-01-21 17:22:54 -0500

    refactor

--- cmd/top-fetch/top-fetch.go
+++ cmd/top-fetch/top-fetch.go
@@ -1,55 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/kingtingthegreat/top-fetch/config"
 	"github.com/kingtingthegreat/top-fetch/env"
+	"github.com/kingtingthegreat/top-fetch/fetch"
 	"github.com/kingtingthegreat/top-fetch/output"
 )
 
 func fetchAndDisplay(web bool) {
 	var imageUrl, trackText string
+	if web {
+		imageUrl, trackText = fetch.WebFetch()
+	} else {
+		imageUrl, trackText = fetch.LocalFetch()
+	}
 
-	// if web {
-	// 	imageUrl, trackText = fetch.WebFetch()
-	// } else {
-	// 	imageUrl, trackText = fetch.LocalFetch()
-	// }
-	// log.Println("converting")
 	ansiImage, err := output.UrlToAnsi(imageUrl)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
 	output.Output(ansiImage, trackText)
 }
 
 func main() {
-	start := time.Now()
-
 	env.LoadEnv()
 	config.ParseArgs()
 	cfg := config.Config()
 
-	fmt.Println("hi from top fetch lalala", start)
 	if cfg.Timeout < 0 {
 		// negative means no timeout
-		fmt.Println(cfg.Web)
+		fetchAndDisplay(cfg.Web)
 	} else {
-		c := make(chan bool)
-		go func() { fmt.Println(cfg.Web); c <- true }()
-		for time.Now().Before(start.Add(time.Duration(cfg.Timeout) * time.Millisecond)) {
-			select {
-			case <-c:
-				return
-			default:
-				continue
-			}
+		timeout := time.Tick(time.Duration(cfg.Timeout) * time.Millisecond)
+		done := make(chan bool)
+		go func() { fetchAndDisplay(cfg.Web); done <- true }()
+		select {
+		case <-done:
+			return
+		case <-timeout:
+			log.Fatal("Exceeded the ", cfg.Timeout, " millisecond time limit")
 		}
-		log.Fatal("Exceeded the ", cfg.Timeout, " millisecond time limit")
 	}
-
 }
