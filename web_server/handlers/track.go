1bfb23312fe0fa1bf97a7b8085d3a56077340fe6
Jeffrey <kingtingthegreat@gmail.com>
2025-03-20 15:51:06 -0400

    logging

--- web_server/handlers/track.go
+++ web_server/handlers/track.go
@@ -46,6 +46,7 @@ func TrackHandler(w http.ResponseWriter, r *http.Request) {
 		db.UpdateUser(user)
 	}
 
+	log.Println("external url: ", track.ExternalUrls.Spotify)
 	link := fmt.Sprintf(" \x1B]8;;%s\x1B\\🎵\x1B]8;;\x1B\\", track.ExternalUrls.Spotify)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(track.Album.Images[0].Url + "\x1d" + track.Name + " - " + track.Artists[0].Name + link))
