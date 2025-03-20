2a634d076c3a4a58485d89313ad2a2a535a43e90
Jeffrey <kingtingthegreat@gmail.com>
2025-03-20 15:47:41 -0400

    refactor

--- output/convert.go
+++ output/convert.go
@@ -1,28 +1,11 @@
 package output
 
 import (
-	"fmt"
 	"image"
-	"net/http"
 
 	"github.com/kingtingthegreat/ansi-converter/converter"
 	"github.com/kingtingthegreat/top-fetch/config"
 )
-
-func UrlToImage(url string) (*image.Image, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get image from url")
-	}
-	defer resp.Body.Close()
-
-	img, _, err := image.Decode(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("cannot decode image from response")
-	}
-
-	return &img, nil
-}
 
 func ImageToAnsi(img *image.Image) string {
 	cfg := config.Config()
