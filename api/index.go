e4aaf17f41f2085b299aeaa490d7298b3ffcd6cc
Jeffrey Ting <jeffrey_ting@cargill.com>
2025-12-12 11:21:00 -0500

    move env logging

--- api/index.go
+++ api/index.go
@@ -1,18 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/kingtingthegreat/top-fetch/web_server/server"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Environment Variables:")
-	for i, env := range os.Environ() {
-		fmt.Println(i, env)
-	}
 	server := server.Server()
 	server.Handler.ServeHTTP(w, r)
 }
